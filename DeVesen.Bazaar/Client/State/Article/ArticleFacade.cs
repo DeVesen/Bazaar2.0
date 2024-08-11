3f6fba30fd1d02969a2e9f0abf364b12491ea0a8
Sven Reichert <Sven.Reichert@generic.de>
2024-08-12 00:58:19 +0200

    Prepare Vendor-Settlement

--- DeVesen.Bazaar/Client/State/Article/ArticleFacade.cs
+++ DeVesen.Bazaar/Client/State/Article/ArticleFacade.cs
@@ -1,4 +1,6 @@
-﻿using DeVesen.Bazaar.Client.Models;
+﻿using DeVesen.Bazaar.Client.Domain;
+using DeVesen.Bazaar.Client.Models;
+using DeVesen.Bazaar.Client.Services;
 using Fluxor;
 
 namespace DeVesen.Bazaar.Client.State.Article;
@@ -6,11 +8,16 @@ namespace DeVesen.Bazaar.Client.State.Article;
 public class ArticleFacade
 {
     private readonly IDispatcher _dispatcher;
+    private readonly ArticleService _articleService;
 
-    public ArticleFacade(IDispatcher dispatcher)
+    public ArticleFacade(IDispatcher dispatcher, ArticleService articleService)
     {
         _dispatcher = dispatcher;
+        _articleService = articleService;
     }
+
+    public async Task<Response> ApproveArticleAsync(long articleNumber)
+        => await _articleService.ApproveAsync(articleNumber);
 
     public void Fetch() => Fetch(new ArticleFilter());
 
