c303fe0188a605f48092dda34f66532eacd6abb2
Sven Reichert <Sven.Reichert@generic.de>
2024-09-13 09:50:52 +0200

    Implement DvSelector, support Add-Not-Exist-Item

---
+++ DeVesen.Bazaar/Client/Components/Manufacturer/DvSelector.razor.cs
@@ -0,0 +1,41 @@
+﻿using MudBlazor;
+
+namespace DeVesen.Bazaar.Client.Components.Manufacturer
+{
+    public partial class DvSelector : MudFormComponent<DvSelector.SelectorItemBase, string>
+    {
+        public DvSelector() : base(new SelectorConverter())
+        {
+            
+        }
+
+
+        public record SelectorItemBase
+        {
+            public required string Caption { get; init; }
+        }
+        public record SelectorItem : SelectorItemBase;
+        public record SelectorItemNew : SelectorItemBase;
+
+        public class SelectorConverter : MudBlazor.Converter<SelectorItemBase, string>
+        {
+            public SelectorConverter()
+            {
+                SetFunc = ToString;
+                GetFunc = ToElement;
+            }
+
+            public static string? ToString(SelectorItemBase? arg)
+            {
+                return arg?.ToString();
+            }
+
+            public static SelectorItemBase? ToElement(string? arg)
+            {
+                return string.IsNullOrWhiteSpace(arg)
+                    ? null
+                    : new SelectorItem { Caption = arg };
+            }
+        }
+    }
+}
---
+++ DeVesen.Bazaar/Client/Components/Manufacturer/ManufacturerSelector.razor.cs
@@ -0,0 +1,12 @@
+﻿using MudBlazor;
+
+namespace DeVesen.Bazaar.Client.Components.Manufacturer
+{
+    public partial class ManufacturerSelector : MudFormComponent<string, string>
+    {
+        public ManufacturerSelector() : base(new DefaultConverter<string>())
+        {
+
+        }
+    }
+}
