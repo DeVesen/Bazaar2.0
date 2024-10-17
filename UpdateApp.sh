091045278c3c1745ac5716b325409937932ded3f
Sven Reichert <Sven.Reichert@generic.de>
2024-10-17 23:09:15 +0200

    any more bach out for SH

--- UpdateApp.sh
+++ UpdateApp.sh
@@ -1,22 +1,22 @@
-echo GIT-Repository
+echo "GIT-Repository"
 
-echo -->> git fetch
+echo "  -->> git fetch"
 git fetch
 
-echo -->> git pull
+echo "  -->> git pull"
 git pull
 
 
-echo Docker
+echo "Docker"
 
-echo docker build bazaar-app-arm
-docker build -t bazaar-app-arm -f ./Dockerfile .
+echo "  -->> docker build bazaar-app-arm"
+docker build -t bazaar-app-arm -f ./Dockerfile . >> docker-build.log
 
-echo -->> Stop 'bazaar-app' Container
+echo "  -->> Stop 'bazaar-app' Container"
 docker stop bazaar-app
 
-echo -->> Remove 'bazaar-app' Container
+echo "  -->> Remove 'bazaar-app' Container"
 docker rm bazaar-app
 
-echo -->> Start 'bazaar-app' Container
+echo "  -->> Start 'bazaar-app' Container"
 docker run -d --name bazaar-app -p 2101:8080 -p 9101:443 -e HTTPS_ACTIVE=true -v "/home/devesen/bazaar-app/data:/app/data" --restart=always bazaar-app-arm
