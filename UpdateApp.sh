4932baa9a375b8bf94b287f0f0c830da4f2895e2
Sven Reichert <Sven.Reichert@generic.de>
2024-10-18 00:47:34 +0200

    Fin output for SH

--- UpdateApp.sh
+++ UpdateApp.sh
@@ -22,3 +22,5 @@ docker rm bazaar-app
 
 echo "  -->> Start 'bazaar-app' Container"
 docker run -d --name bazaar-app -p 2101:8080 -p 9101:443 -e HTTPS_ACTIVE=true -v "/home/devesen/bazaar-app/data:/app/data" --restart=always bazaar-app-arm
+
+echo "HABE NUN FERTIG :-)"
